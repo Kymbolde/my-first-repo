a28fe37f6fe26d23fc55407414788a32db7d1196
Brayden Kimball <Brayden@Rochelles-MacBook-Pro.local>
2016-09-12 14:53:12 -0600

    status update 2

--- journal.js
+++ journal.js
@@ -1,3 +1,3 @@
 var journal1 = [
-  "Excited to start learning!", "So far things are going pretty well."
+  "Excited to start learning!", "So far things are going pretty well.", "I really feel like I'm starting to get the hang of this. Can't wait to learn more."
 ]
